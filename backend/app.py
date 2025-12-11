8b6b384c4ff3f45c5a66d3c3e11c537ec742d996
Amirali <143319842+CodeWASD@users.noreply.github.com>
2025-12-11 23:32:05 +0330

    feat: connect auth routes to flask app

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from backend.routes.auth import auth
 
 app = Flask(__name__)
+app.register_blueprint(auth)
 
 @app.route("/")
 def home():
@@ -8,3 +10,4 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
