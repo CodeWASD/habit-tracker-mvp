3a06bfa19d0aa07dacba3e33a7e9928ab551394f
Amirali <ahmadiamirali2004@gmail.com>
2025-12-21 18:33:05 +0330

    chore: remove redundant json files

--- backend/storage.py
+++ backend/storage.py
@@ -2,20 +2,19 @@ import json
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
-DB_DIR = BASE_DIR / "database"
-
-USERS_FILE = DB_DIR / "users.json"
-HABITS_FILE = DB_DIR / "habits.json"
+DATA_DIR = BASE_DIR / "data"
+DB_FILE = DATA_DIR / "db.json"
 
 
-def read_json(file_path):
-    if not file_path.exists():
-        return []
-    with open(file_path, "r", encoding="utf-8") as f:
+def read_db():
+    if not DB_FILE.exists():
+        return {"users": [], "habits": []}
+
+    with open(DB_FILE, "r", encoding="utf-8") as f:
         return json.load(f)
 
 
-def write_json(file_path, data):
-    with open(file_path, "w", encoding="utf-8") as f:
+def write_db(data: dict):
+    with open(DB_FILE, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
-س
+
